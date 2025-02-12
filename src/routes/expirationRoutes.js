714d95f7f391d078afadd4af6949af2bfd47607e
CinthiaDayanara <cdchuraco@uce.edu.ec>
2025-02-11 20:49:35 -0500

    prueba

--- src/routes/expirationRoutes.js
+++ src/routes/expirationRoutes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const { getCards, deleteCard } = require("../controllers/expirationController");  // Importa el controlador
 const router = express.Router();
 
-// Ruta para obtener todas las tarjetas
-router.get("/", getCards);
 
 // Ruta para eliminar una tarjeta
 router.delete("/:id", deleteCard);  // :id es el parámetro que recibe el ID de la tarjeta 
